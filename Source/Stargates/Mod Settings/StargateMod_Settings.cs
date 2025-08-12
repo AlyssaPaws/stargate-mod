2c6997ad40c70e93cac31e6650f105b694e3429a
Ashyfox <ashenfoxl@outlook.com>
2025-08-12 15:18:19 +0200

    Settings basic setup

---
+++ Source/Stargates/Mod Settings/StargateMod_Settings.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Verse;
+
+namespace StargatesMod.Mod_Settings
+{
+    public class StargatesMod_Settings : ModSettings
+    {
+        /*Mod Settings*/
+
+        /*Write Settings to file*/
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+    }
+}
---
+++ Source/Stargates/Mod Settings/StargatesMod_Mod.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace StargatesMod.Mod_Settings
+{
+    public class StargatesMod_Mod : Mod
+    {
+        /*Reference to settings*/
+        StargatesMod_Settings _settings;
+        
+        /*Resolve settings reference*/
+        public StargatesMod_Mod(ModContentPack content) : base(content)
+        {
+            _settings = GetSettings<StargatesMod_Settings>();
+        }
+
+
+        /*Settings GUI*/
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            
+            /*Section 1*/
+            Listing_Standard sec1 = listingStandard.BeginSection(150);
+            sec1.Label("SGM_SettingsCat_Main".Translate());
+            sec1.GapLine();
+            /*sec1.Label("Setting2");
+            sec1.Label("Setting3");
+            sec1.Label("Setting4");*/
+            listingStandard.EndSection(sec1);
+
+            listingStandard.Gap();
+            
+            /*/*Section 2#1#
+            Listing_Standard sec2 = listingStandard.BeginSection(150);
+            sec2.Label("Test Section 2");
+            sec2.GapLine();
+            sec2.Label("Setting1");
+            sec2.Label("Setting2");
+            sec2.Label("Setting3");
+            sec2.Label("Setting4");
+            listingStandard.EndSection(sec2);
+            
+            listingStandard.Gap();*/
+            
+            /*Toggleable patches notice*/
+            Listing_Standard SecTP = listingStandard.BeginSection(75);
+            SecTP.Label("SGM_ToggleablePatchesHeader".Translate());
+            SecTP.GapLine();
+            SecTP.Label("SGM_ToggleablePatchesText".Translate());
+            listingStandard.EndSection(SecTP);
+            
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        /*Settings mod label*/
+        public override string SettingsCategory()
+        {
+            return "SGM_StargatesMod".Translate();
+        }
+    }
+}
